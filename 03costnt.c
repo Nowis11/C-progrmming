09623f57d0eee4cc21597adb8c42fbcdcc316dd4
Nowis11 <85983856+Nowis11@users.noreply.github.com>
2021-12-22 14:26:42 +0545

    Add files via upload

---
+++ 03costnt.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#define PI 3.14
+/*
+vribles: something tht store vlue;
+rules : cannot start with numbers ,symbols
+
+constnt: once cannot change
+
+*/
+int main(){
+
+         int firstNumber = 10;
+        printf("The first number is %d.\n",firstNumber);
+        printf("The vlue of PI is %.4f.",PI);
+        return 0;	
+	
+}
