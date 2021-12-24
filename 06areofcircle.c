e25005d0e0f375e9027f015dcf511301a7b793bc
Nowis11 <85983856+Nowis11@users.noreply.github.com>
2021-12-24 18:09:24 +0545

    Add files via upload

---
+++ 06areofcircle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define PI 3.14
+int main(){
+//WAP IN C THAT FIND AREA OF CIRCLE PI*RSquare
+
+      float radious,area; 
+      printf("Enter Radious:\n");
+      scanf("%f",&radious);
+      
+      area = PI * radious *  radious;
+      printf("The area of circle is %f",area);
+return 0;
+}
